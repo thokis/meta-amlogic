517ea6bb320a9c5a825a6313759cb750c89b67a9
Jerome Brunet <jbrunet@baylibre.com>
2022-05-24 13:53:09 +0200

    wic: remove nosem specific plugins
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- scripts/lib/wic/plugins/source/boot-mbr.py
+++ scripts/lib/wic/plugins/source/boot-mbr.py
@@ -1,10 +1,10 @@
 #
-# Copyright (c) 2020, Baylibre SAS
+# Copyright (c) 2022, Baylibre SAS
 #
 # SPDX-License-Identifier: GPL-2.0
 #
 # DESCRIPTION
-# This implements the 'bootsd-aml' source plugin class for 'wic'
+# This implements the 'boot-mbr' source plugin class for 'wic'
 #
 # AUTHORS
 # Jerome Brunet <jbrunet@baylibre.com>
@@ -20,38 +20,44 @@ from wic.misc import (exec_cmd, get_bitbake_var)
 
 logger = logging.getLogger('wic')
 
-class BootSdAmlPlugin(SourcePlugin):
+class BootMbrPlugin(SourcePlugin):
     """
-    Write the ATF on the SDcard for the AML ROM Code
+    Write a binary in LBA 0, preserving the MBR
     """
 
-    name = 'bootsd-aml'
+    name = 'boot-mbr'
 
     @classmethod
     def do_install_disk(cls, disk, disk_name, creator, workdir, oe_builddir,
                         bootimg_dir, kernel_dir, native_sysroot):
         """
         Called after all partitions have been prepared and assembled into a
-        disk image. In this case, we install the ATF.
+        disk image.
         """
         if creator.ptable_format == 'gpt':
             """
-            Bootloader will need the 2nd LBA - not compatible with GPT
+            The zone should be reversed for the protective MBR
             """
             raise WicError("Unsupported partition table: %s" %
                            creator.ptable_format)
 
         deploy_dir = get_bitbake_var("DEPLOY_DIR_IMAGE")
-        atf = os.path.join(deploy_dir, "atf.bin.sd.bin")
+        mbrname = get_bitbake_var("DEPLOY_MBR_NAME")
+
+        if not mbrname:
+            mbrname = "mbr.bin"
+
+        mbrfile = os.path.join(deploy_dir, mbrname)
+        if not os.path.exists(mbrfile):
+            raise WicError("Couldn't find %s. use DEPLOY_MBR_NAME to specify "
+                           "the name of the mbr binary" % mbrfile)
+
+        # TODO: check size
         full_path = creator._full_path(workdir, disk_name, "direct")
 
-        logger.debug("Installing ATF on SD disk %s as %s with size %s bytes",
-                     disk_name, full_path, disk.min_size)
+        logger.debug("Installing MBR %s on disk %s as %s with size %s bytes",
+                     mbrfile, disk_name, full_path, disk.min_size)
 
         # Write LBA 0 part preserving MSDOS partition table
-        dd_cmd = "dd if=%s of=%s conv=fsync,notrunc bs=1 count=440" % (atf, full_path)
-        exec_cmd(dd_cmd, native_sysroot)
-
-        # Write the rest starting from LBA 1
-        dd_cmd = "dd if=%s of=%s conv=fsync,notrunc bs=512 skip=1 seek=1" % (atf, full_path)
+        dd_cmd = "dd if=%s of=%s conv=fsync,notrunc bs=1 count=440" % (mbrfile, full_path)
         exec_cmd(dd_cmd, native_sysroot)
