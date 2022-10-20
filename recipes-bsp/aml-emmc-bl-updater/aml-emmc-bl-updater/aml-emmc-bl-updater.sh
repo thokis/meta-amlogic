a48843eff4b6ce9216ab8ce553b1a169aad25c2e
Jerome Brunet <jbrunet@baylibre.com>
2022-10-20 09:54:34 +0200

    aml-emmc-bl-updater: selectively disable the user partition
    Only disable the user paritition if the device was booted from.
    If not, the device may be using GPT partitioning. Erase the 1st
    sector would break it
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- recipes-bsp/aml-emmc-bl-updater/aml-emmc-bl-updater/aml-emmc-bl-updater.sh
+++ recipes-bsp/aml-emmc-bl-updater/aml-emmc-bl-updater/aml-emmc-bl-updater.sh
@@ -18,20 +18,34 @@ errcho() {
     >&2 echo $@;
 }
 
+get_bootenabled() {
+    PARTCONF=`mmc extcsd read ${1} | grep "PARTITION_CONFIG:" | grep -o -e "0x\([a-fA-F0-9]\)\{2\}"`
+    echo "$(( (${PARTCONF} >> 3) & 0x7 ))"
+}
+
 get_device() {
-    #1 is the eMMC
+    #1 boot currently enabled
     #2 index of the device
 
-    PARTCONF=`mmc extcsd read ${1} | grep "PARTITION_CONFIG:" | grep -o -e "0x\([a-fA-F0-9]\)\{2\}"`
+    case "${1}" in
+	0|7|2) # Treat user and boot1 in the same fashion
+	    SLOT="1"
+	    ;;
+	1)
+	    SLOT="0"
+	    ;;
+	*)
+	    errcho "Unsupported active slot ${1}"
+	    exit 1
+	    ;;
+    esac
 
-    # Convoluted sh way to do an XOR between the bootpart and the index
-    # In the end BOOTPART contains 0 is the boot hw part was boot0, simple :P
-    ! [ "${PARTCONF}" = "0x10" ]; BOOTPART=$?
-    if [ "${BOOTPART}" = "${2}" ]; then
+    if [ "${SLOT}" = "${2}" ]; then
 	echo "boot0"
     else
 	echo "boot1"
     fi
+
 }
 
 block_size() {
@@ -69,7 +83,7 @@ update_boot() {
     MD5EMMC=`dd if=${1} bs=512 skip=1 count="$(block_size ${2})" 2>/dev/null | md5sum | cut -d ' ' -f0)`
 
     if [ "${MD5EMMC}" != "$(md5sum ${2} | cut -d ' ' -f0)" ]; then
-       errcho "${1} update failure" 
+       errcho "${1} update failure"
 
        # Erase the first sector on failure so the ROMCode does not even try
        dd if=/dev/zero of=${1} seek=1 bs=512 count=1 conv=notrunc 2>/dev/null
@@ -81,13 +95,20 @@ update_boot() {
 
 finalize_update() {
     # invalidate the bootloader in the user part
-    dd if=/dev/zero of=${1} seek=1 bs=512 count=1 conv=notrunc 2>/dev/null
+    case "${2}" in
+	0|7)
+	    # Invalidate the user part if it was the boot source
+	    errcho "Disabled user partition"
+	    dd if=/dev/zero of=${1} seek=1 bs=512 count=1 conv=notrunc 2>/dev/null
+	    ;;
+    esac
 
     errcho "Update done"
 }
 
 # TODO: Check reliable write is active - safety depends on it
 DEVICE=${1}
+BOOTENABLED="$(get_bootenabled ${1})"
 
 # Why do we bother making the file slightly bigger ?
 # Because doing so allows to read it back by (512B) blocks
@@ -100,11 +121,11 @@ trap "cleanup_on_exit" EXIT
 # Update the partition we are not using first. We got here
 # so the one we are using must be working somehow
 # This is important if the update gets repeatidly interrupted
-update_boot "${1}$(get_device ${1} 1)" ${TMPATF}
+update_boot "${1}$(get_device ${BOOTENABLED} 1)" ${TMPATF}
 
 # Update the one we are using after that
-update_boot "${1}$(get_device ${1} 0)" ${TMPATF}
+update_boot "${1}$(get_device ${BOOTENABLED} 0)" ${TMPATF}
 
 # Make sure we don't use the user part anymore
 # and mark the update as done
-finalize_update ${1}
+finalize_update ${1} ${BOOTENABLED}
